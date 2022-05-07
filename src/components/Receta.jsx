85f2605acfabc6fe5d8304c55965c1fea571d3e3
Camilo Garzón <Camilogzn19@gmail.com>
2022-05-07 17:15:56 -0500

    fixing errors in Material UI

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -1,7 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
 
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import Typography from '@mui/material/Typography';
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 600,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
+
 const Receta = ({ receta }) => {
+  // Configuración del modal De MUI
+  const [open, setOpen] = useState(false);
+
+  console.log(style);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   // Extraer los valores del context
   const { guardarIdReceta } = useContext(ModalContext);
 
@@ -20,10 +48,22 @@ const Receta = ({ receta }) => {
             className="btn btn-block btn-primary"
             onClick={() => {
               guardarIdReceta(receta.idDrink);
+              handleOpen();
             }}
           >
             Ver Receta
           </button>
+
+          <Modal open={open}>
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Desde modal
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+              </Typography>
+            </Box>
+          </Modal>
         </div>
       </div>
     </div>
